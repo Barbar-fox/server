166ee4e25f71df0422f6e60117757cc40663f1db
GanangPrakoso <ganangmanggolo@gmail.com>
2020-02-07 00:09:49 +0700

    authorization done

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,1 +1,27 @@
 const { UserHotel } = require('../models')
+
+module.exports = (req, res, next) {
+   UserHotel.findOne({
+      where : {
+         id : req.params.id
+      }
+   })
+      .then (data => {
+         // console.log(data);
+         
+         if (!data) {
+            next({code : 404, message: `there's no Hotel with id ${req.params.id}`})
+         }
+         else {
+            if (data.UserId !== req.currentUserId) {
+               next({code : 401})
+            }
+            else {
+               next()
+            }
+         }
+      })
+      .catch (err => {
+         next(err)
+      })
+}
