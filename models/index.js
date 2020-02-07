e4ab9999f9cfd0aea02a011d7699cff77afa5b9c
uulwake <uulwake@example.com>
2020-02-07 13:49:42 +0700

    Email Constraint: Done

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-config.password = process.env.DEVELOPMENT_PASSWORD
+// config.password = process.env.DEVELOPMENT_PASSWORD
 
 let sequelize;
 if (config.use_env_variable) {
